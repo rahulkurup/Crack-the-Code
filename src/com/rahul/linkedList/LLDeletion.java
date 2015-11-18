d5926a99375e9cfe26c8d1b7fcf3a23b66260a42
rahulkurup <formyrahul@gmail.com>
2015-11-18 11:54:09 +0530

    Linked List Deletion
    Linked List Deletion

--- src/com/rahul/linkedList/LLDeletion.java
+++ src/com/rahul/linkedList/LLDeletion.java
@@ -5,7 +5,41 @@ public class LLDeletion {
 	
 	
 	public static void main(String[] args) {
-		SampleLinkedList ll = new SampleLinkedList(10);
+		SampleLinkedList ll = new SampleLinkedList(11);
+		delete(ll,5);
 		ll.printList();		
+	}
+	
+	/**
+	 * @param ll
+	 * @param d
+	 */
+	public static void delete(SampleLinkedList ll, int d) {
+
+		if(ll.getHead() == null) {
+			System.out.println("Not Valid");
+			return;
+		} 
+
+		Node previous = null;
+		Node current = ll.getHead();
+		if(current.getData() == d) {
+			ll.setHead(current.getNext());
+			return;
+		} else {
+			previous = current;
+			current = current.getNext();
+		}
+
+		while(current !=null) {
+			if(current.getData() == d) {
+				previous.setNext(current.getNext());
+				return;
+			} else {
+				previous = current;
+				current = current.getNext();
+			}
+		}
+
 	}
 }
--- src/com/rahul/linkedList/SampleLinkedList.java
+++ src/com/rahul/linkedList/SampleLinkedList.java
@@ -41,35 +41,7 @@ public class SampleLinkedList {
 		Node current = this.getHead();
 		while(current != null) {
 			System.out.println(current.getData());		
-			current = current.next;
+			current = current.getNext();
 		}		
-	}
-
-
-	static class Node {
-
-		private int data;
-		private Node next;
-
-		public Node(int data) {
-			this.data = data;
-		}
-
-		public int getData() {
-			return data;
-		}
-
-		public void setData(int data) {
-			this.data = data;
-		}
-
-		public Node getNext() {
-			return next;
-		}
-
-		public void setNext(Node next) {
-			this.next = next;
-		}		
-
 	}
 }
