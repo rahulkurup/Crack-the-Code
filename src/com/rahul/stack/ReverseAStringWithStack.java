0f40e9badf63e40af8f90dba0c968ee029a3880d
rahulkurup <formyrahul@gmail.com>
2015-11-26 15:21:28 +0530

    Reverse String using Stack
    Reverse String using Stack

--- src/com/rahul/stack/ReverseAStringWithStack.java
+++ src/com/rahul/stack/ReverseAStringWithStack.java
@@ -20,13 +20,14 @@ public class ReverseAStringWithStack {
 	public static String reverse(String input) {
 
 		Stack<Character> stack = new Stack<Character>();
-
+		// Go through each character and push into array
 		for(char c : input.toCharArray()) {
 			stack.push(c);
 		}
 
 		StringBuffer output = new StringBuffer();
-
+		
+		//Pop all characters and append it into output
 		while(!stack.isEmpty()) {
 			output.append(stack.pop());
 		}
